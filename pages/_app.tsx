bebd0421728fdfa4d4b15b0bd9e5181c4fb951eb
EllenSouza <ellen.almeida.souza21@gmail.com>
2022-11-16 23:35:51 -0300

    add responsive style to headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,9 @@ import type { AppProps } from "next/app";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
-
 import "/node_modules/primeflex/primeflex.css";
+import "../styles/layout.css";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

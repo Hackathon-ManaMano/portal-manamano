f3a7a3dc4f9159742d0e55b126efdd192dee47bf
keviinsna <keevinsenna2001@gmail.com>
2022-11-25 01:16:59 -0300

    refatora onclick

--- components/HeaderPublic.tsx
+++ components/HeaderPublic.tsx
@@ -4,39 +4,39 @@ import { Menubar } from "primereact/menubar";
 import { Button } from "primereact/button";
 import { useRouter } from "next/router";
 
-export default function HeaderPublic(){
+export default function HeaderPublic() {
+  const router = useRouter();
+  const items = [
+    {
+      label: "Sobre Nós",
+    },
+    {
+      label: "Quem Apoiamos",
+    },
+    {
+      label: "Contribua",
+    },
+  ];
 
-    const router = useRouter();
-    const items = [
-        {
-          label: "Sobre Nós",
-        },
-        {
-          label: "Quem Apoiamos",
-        },
-        {
-          label: "Contribua",
-        },
-      ];
-    
-      const start = (
-        <>
-          <Image
-            src="/logo-manamano.png"
-            alt="Logo ManaMano"
-            height={40}
-            width={120}
-          />
-        </>
-      );
-    
-      const end = (
-        <>
-          <Button className="p-button-primary p-button-sm mr-1" label="Entrar" onClick={ (event)=> {event.preventDefault(); router.push('/login')}} />
-        </>
-      );
-    return (      
-    
-        <Menubar model={items} start={start} end={end} />
-    );
-};
+  const start = (
+    <>
+      <Image
+        src="/logo-manamano.png"
+        alt="Logo ManaMano"
+        height={40}
+        width={120}
+      />
+    </>
+  );
+
+  const end = (
+    <>
+      <Button
+        className="p-button-primary p-button-sm mr-1"
+        label="Entrar"
+        onClick={() => router.push("/login")}
+      />
+    </>
+  );
+  return <Menubar model={items} start={start} end={end} />;
+}

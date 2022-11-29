34ba29ed4ba4823be4851bbbfbf9acf35c48d414
keviinsna <keevinsenna2001@gmail.com>
2022-11-29 00:48:31 -0300

    adiciona pasta utils

---
+++ utils/constants.ts
@@ -0,0 +1,12 @@
+export default class C {
+  // SERVER RESPONSE
+  static readonly SERVER_SUCCESS: string = "success";
+  static readonly SERVER_WARN: string = "warn";
+  static readonly SERVER_ERROR: string = "error";
+
+  static readonly LOGIN_INVALIDO: string = "Email ou senha incorretos";
+  static readonly FEEDBACK_PADRAO: string = "Por favor, tente novamente.";
+
+  // Register
+  static readonly USUARIO_CRIADO: string = "Usuário criado com sucesso.";
+}

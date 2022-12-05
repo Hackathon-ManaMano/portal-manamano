9975e4274817326cac1d9c5e654b38ce3e7251de
Riquelme Gomes <riquelmefgomes@gmail.com>
2022-12-05 15:58:11 -0300

    Implementa responsividade

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,50 +7,72 @@ export default function Home() {
   return (
     <>
       <div className="text-indigo-900">
-        <div
-          className="flex flex-column justify-content-center align-items-center"
+        
+        <div  
+          className="flex flex-column bg-bluegray-300 justify-content-center align-items"
           style={{
-            height: "500px",
-            width: "100%",
-            position: "relative",
-            overflow: "hidden",
-          }}
-        >
+          position: "relative",
+          overflow: "hidden"}}>
+
           <Image
-            className="opacity-70 shadow-8"
+            className="opacity-70"
             fill
             src="/home-clareada.jpg"
             alt="Foto Fundo ManaMano"
-            style={{ objectFit: "cover" }}
+            style={{ objectFit:"cover"}}
             priority
-            quality={100}
+            quality={100} 
           />
 
-          <div
-            className="flex flex-column justify-content-center align-items-center gap-7 "
-            style={{ position: "absolute" }}
-          >
-            <Image
-              src="/logo/logo-completo-2.png"
-              width={317}
-              height={240}
-              alt="Logotipo ManaMano"
-              style={{ objectFit: "cover" }}
-              priority
-            />
+          <div 
+            className="flex flex-column justify-content-center align-items-center gap-4 z-5"
+            style={{}}>
+            
+            <div className="field flex col-12 justify-content-center mt-6">
+              <Image
+                src="/logo/logo-completo-2.png"
+                width={305}
+                height={240}
+                alt="Logotipo ManaMano"
+                priority />
+            </div>
 
-            <p className=" text-center text-3xl font-semibold col-8 line-height-3">
-              Ao apoiar o Fundo MANAMANO você contribui para a formação e o
-              fomento de nano e micro empreendedores das periferias do Rio de
-              Janeiro, criando oportunidades de desenvolvimento de negócios e
-              geração de renda para além da crise da Covid-19.
-            </p>
+            <div className="text-center col-8 mb-6">
+              <p className="text-3xl font-semibold line-height-3">
+                Ao apoiar o Fundo MANAMANO você contribui para a formação e o
+                fomento de nano e micro empreendedores das periferias do Rio de
+                Janeiro, criando oportunidades de desenvolvimento de negócios e
+                geração de renda para além da crise da Covid-19.
+              </p>
+            </div>
+            
           </div>
+      
         </div>
 
-        <div className="flex px-7 mt-7 align-items-center justify-content-center gap-2">
+        {/* <div 
+          className="flex flex-column bg-cover justify-content-center align-items-center" 
+          style={{background: `url(${"/home-clareada.jpg"})`}}>
+
+          <div className="field flex col-12 justify-content-center mt-6">
+            <Image src="/logo/logo-completo-2.png" width={305} height={240} alt="Logotipo ManaMano"/>
+          </div>
+
+          <div className="text-center col-8 mt-4 mb-6">
+              <p className="text-3xl font-semibold line-height-3"> Ao apoiar o Fundo MANAMANO você contribui para a formação
+              e o fomento de nano e micro empreendedores das periferias do Rio de Janeiro, criando oportunidades de 
+              desenvolvimento de negócios e geração de renda para além da crise da Covid-19. </p>
+          </div>
+        
+      </div> */}
+
+
+        <div className="px-7 mt-7 gap-2">
+          
+          <div className="flex flex-column md:flex-row align-items-center justify-content-center gap-5">
+
           <Image
-            className=" shadow-8"
+            className="col-12 md:col-6 shadow-8"
             width={617}
             height={320}
             src="/home-colorida.jpg"
@@ -60,9 +82,10 @@ export default function Home() {
             quality={100}
           />
 
-          <div className=" ml-5">
-            <p className="text-center text-5xl font-bold ">QUEM SOMOS?</p>
-            <p className="mt-4 text-justify text-2xl line-height-3 ">
+          <div className="flex flex-column align-items-center justify-content-center col-12 md:col-6">
+
+            <p className="text-center text-4xl md:text-5xl font-bold pt-5">QUEM SOMOS?</p>
+            <p className="mt-4 text-justify text-2xl line-height-3">
               O MANAMANO é um fundo de transformação social e de desenvolvimento
               destinado a fortalecer negócios de nano e micro empreendedoras(es)
               de favelas e de periferias da Região Metropolitana do Rio de
@@ -74,25 +97,35 @@ export default function Home() {
               instituições de fellows Ashoka (Asplande, Saúde Criança, Luta pela
               Paz, Gastromotiva) e a UFRJ.
             </p>
+
           </div>
+
+          </div>
+
         </div>
 
-        <div className="flex px-7 mt-7 mb-6 align-items-center justify-content-center gap-2">
-          <div className=" mr-5">
-            <p className="text-center text-5xl font-bold">
-              COM QUEM TRABALHAMOS?
-            </p>
-            <p className="mt-4 text-justify text-2xl line-height-3">
-              O Fundo trabalha com nano e micro empreendedoras(es) de regiões
-              periféricas, que fazem parte de instituições lideradas por Fellows
-              da Ashoka no Rio de Janeiro. Na primeira fase, até 700
-              empreendedoras(es) serão beneficiada(os). Serão considerados
-              empreendedores(as) formais e informais.
-            </p>
-          </div>
+
+        <div className="px-7 mt-8 mb-6">
+          
+          <div className="flex flex-column md:flex-row align-items-center justify-content-center">
+
+            <div className="flex flex-column col-12 md:col-6 ">
+
+              <p className="text-center text-4xl md:text-5xl font-bold">
+                COM QUEM TRABALHAMOS?
+              </p>
+              <p className="mt-4 text-justify text-2xl line-height-3">
+                O Fundo trabalha com nano e micro empreendedoras(es) de regiões
+                periféricas, que fazem parte de instituições lideradas por Fellows
+                da Ashoka no Rio de Janeiro. Na primeira fase, até 700
+                empreendedoras(es) serão beneficiada(os). Serão considerados
+                empreendedores(as) formais e informais.
+              </p>
+
+            </div>
 
           <Image
-            className=" shadow-8 "
+            className="col-12 md:col-6 shadow-8 ml-4"
             width={617}
             height={320}
             src="/empreendedoras-feira.jpg"
@@ -102,100 +135,12 @@ export default function Home() {
             priority
             quality={100}
           />
+
+          </div>
+
         </div>
+
       </div>
-
-      {/* <div
-        className="bg-cover"
-        style={{
-          height: "100%",
-          width: "100%",
-          position: "relative",
-          overflow: "hidden",
-        }}
-      >
-        <Image
-          className="shadow-8"
-          fill
-          src="/home-clareada.jpg"
-          alt="Foto Fundo ManaMano"
-          style={{ objectFit: "cover" }}
-        />
-
-        <div className="field flex col-12 justify-content-center mt-6">
-          <Image
-            src="/logo/logo-completo-2.png"
-            width={317}
-            height={240}
-            alt="Logotipo ManaMano"
-          />
-        </div>
-
-        <div className="field col-8 col-offset-2 text-center font-bold">
-          <p className="text-2xl ">
-            {" "}
-            Ao apoiar o Fundo MANAMANO você contribui para a formação e o
-            fomento de nano e micro empreendedores das periferias do Rio de
-            Janeiro, criando oportunidades de desenvolvimento de negócios e
-            geração de renda para além da crise da Covid-19.{" "}
-          </p>
-        </div>
-      </div>
-
-      <div className="grid p-4 mt-5 ml-6 align-items-center">
-        <Image
-          className="shadow-8"
-          width={617}
-          height={320}
-          src="/home-colorida.jpg"
-          alt="Foto Fundo ManaMano"
-          style={{ objectFit: "cover", borderRadius:"10px" }}
-          sizes="(max-width: 320px) 280px,(max-width: 480px)440px,800px"
-        />
-
-        <div className="col-12 md:col-6 flex flex-column text-center">
-          <p className="text-3xl font-bold underline"> QUEM SOMOS? </p>
-          <p className="mt-3 ml-5 text-justify text-xl">
-            O MANAMANO é um fundo de transformação social e de desenvolvimento
-            destinado a fortalecer negócios de nano e micro empreendedoras(es)
-            de favelas e de periferias da Região Metropolitana do Rio de
-            Janeiro, como forma de mitigar os impactos econômicos causados pela
-            Covid-19 e criar ferramentas de geração de renda para famílias
-            vulneráveis. Seu objetivo é oferecer suporte financeiro, formação e
-            consultoria em gestão, para o aperfeiçoamento de iniciativas
-            empreendedoras(es). É fruto de uma parceria entre instituições de
-            fellows Ashoka (Asplande, Saúde Criança, Luta pela Paz,
-            Gastromotiva) e a UFRJ.{" "}
-          </p>
-        </div>
-      </div>
-
-      <div className="grid p-4 align-items-center gap-4 ml-6">
-        <div className="col-12 md:col-6 flex flex-column text-xl text-center">
-          <p className="text-3xl font-bold underline">
-            {" "}
-            COM QUEM TRABALHAMOS?{" "}
-          </p>
-          <p className="mt-3 text-xl text-justify">
-            {" "}
-            O Fundo trabalha com nano e micro empreendedoras(es) de regiões
-            periféricas, que fazem parte de instituições lideradas por Fellows
-            da Ashoka no Rio de Janeiro. Na primeira fase, até 700
-            empreendedoras(es) serão beneficiada(os). Serão considerados
-            empreendedores(as) formais e informais.{" "}
-          </p>
-        </div>
-
-        <Image
-          className="shadow-8 "
-          width={617}
-          height={320}
-          src="/empreendedoras-feira.jpg"
-          alt="Empreendedoras em evento"
-          style={{ objectFit: "cover", borderRadius: "50%" }}
-          sizes="(max-width: 320px) 280px,(max-width: 480px)440px,800px"
-        />
-      </div> */}
     </>
   );
 }
@@ -207,3 +152,4 @@ Home.getLayout = function getLayout(page: ReactElement) {
     </>
   );
 };
+''
